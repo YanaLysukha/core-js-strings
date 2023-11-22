55031d9bff3c760b0ca34826088c4057527cbe23
YanaLysukha <yanina.lysukha@gmail.com>
2023-11-22 18:57:59 +0100

    feat: implement reverse words function

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -371,8 +371,9 @@ function findLongestWord(/* sentence */) {
  *   reverseWords('Hello World') => 'olleH dlroW'
  *   reverseWords('The Quick Brown Fox') => 'ehT kciuQ nworB xoF'
  */
-function reverseWords(/* str */) {
-  throw new Error('Not implemented');
+function reverseWords(str) {
+  const currentArr = str.split(' ');
+  return currentArr.map((word) => word.split('').reverse().join('')).join(' ');
 }
 
 /**
