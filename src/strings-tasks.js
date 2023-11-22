488b46e78585e6961b10c53874fb892e2230b999
YanaLysukha <yanina.lysukha@gmail.com>
2023-11-22 20:56:04 +0100

    feat: implement find longest word function

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -370,8 +370,10 @@ function isPalindrome(str) {
  *   findLongestWord('A long and winding road') => 'winding'
  *   findLongestWord('No words here') => 'words'
  */
-function findLongestWord(/* sentence */) {
-  throw new Error('Not implemented');
+function findLongestWord(sentence) {
+  const arr = sentence.split(' ');
+  const sortedArr = arr.sort((a, b) => b.length - a.length);
+  return sortedArr[0];
 }
 
 /**
