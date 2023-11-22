0d30b5ae61a50dffb49b094d1b27b565627dc1ea
YanaLysukha <yanina.lysukha@gmail.com>
2023-11-22 16:32:33 +0100

    feat: implement order alphabetically function

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -277,8 +277,9 @@ function reverseString(str) {
  *   orderAlphabetically('textbook') => 'bekoottx'
  *   orderAlphabetically('abc123xyz') => '123abcxyz'
  */
-function orderAlphabetically(/* str */) {
-  throw new Error('Not implemented');
+function orderAlphabetically(str) {
+  const sortedChars = str.split('').sort();
+  return sortedChars.join('');
 }
 
 /**
